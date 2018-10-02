1766909e6171774e1687ecb971cf9226e81535e5
tnolf2018 <thomas.nolf@hotmail.fr>
2018-10-02 09:26:07 +0200

    Ajout des activités principales

---
+++ app/src/main/java/fr/eni/projetlokacar/activities/clients/GestionClientsActivity.java
@@ -0,0 +1,15 @@
+package fr.eni.projetlokacar.activities.clients;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import fr.eni.projetlokacar.R;
+
+public class GestionClientsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gestion_clients);
+    }
+}
