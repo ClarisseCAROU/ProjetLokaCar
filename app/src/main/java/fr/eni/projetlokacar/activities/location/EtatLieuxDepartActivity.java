82101b8abb061bffe6d85d2fab8ec24ac13d59b5
ClarisseCAROU <clarisse.caroupanapoulle2018@campus-eni.fr>
2018-10-03 16:09:56 +0200

    Activité état des lieux départ (vide)

---
+++ app/src/main/java/fr/eni/projetlokacar/activities/location/EtatLieuxDepartActivity.java
@@ -0,0 +1,17 @@
+package fr.eni.projetlokacar.activities.location;
+
+
+import android.os.Bundle;
+
+import fr.eni.projetlokacar.R;
+import fr.eni.projetlokacar.activities.BaseActivity;
+
+public class EtatLieuxDepartActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_etat_lieux_depart);
+    }
+
+}
